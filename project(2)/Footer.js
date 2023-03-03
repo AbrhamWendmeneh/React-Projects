d09baddbd3ef2002310d36cc6e07e5061b74d74b
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-03-03 22:45:13 +0300

    Add files via upload

---
+++ project(2)/Footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className='footer-main'>
+            
+            
+            <img className='wimg_1' src='linkedin_icon.png'/>
+            <img className='wimg' src='whatsapp_icon.png'/>
+            
+            
+            </div>
+            
+        </footer>
+    )
+}
